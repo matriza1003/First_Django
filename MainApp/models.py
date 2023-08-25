c3d00d6afb075af6d9af70f64161c72183167131
matriza1003 <matriza1996@gmail.com>
2023-08-25 23:18:53 +0300

    Final end

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+class Color(models.Model):
+    name = models.CharField(max_length=32)
 
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100,default="базовое описание")
+    colors = models.ManyToManyField(to= Color)
 
 class Image(models.Model):
     title = models.CharField(max_length=200)
